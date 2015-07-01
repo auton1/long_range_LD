aae1a2d7ab814b1612aca12931719d21dbbf64ca
auton1 <adam.auton@gmail.com>
2015-07-01 12:36:29 -0400

    Initial Commit

---
+++ long_range_LD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  long_range_LD
+//
+//  Created by Adam Auton on 1/07/15.
+//  Copyright (c) 2015 Adam Auton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
